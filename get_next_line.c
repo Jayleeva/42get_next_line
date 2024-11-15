b4faa90805ac722a9e198eeb8b56cb17fd3f7af4
0120 <a0120@iMac-de-0120.local>
2024-11-15 15:50:22 +0100

    les vraies bases

--- get_next_line.c
+++ get_next_line.c
@@ -1,20 +1,99 @@
 #include "get_next_line.h"
 
-//read(int fd, void *buf, size_t count)
+static char *update_buffer(buffer)
+{
+    int i;
+    int j;
+    char    *line;
+
+    i = 0;
+    while (buffer[i] && buffer[i] != '\n')
+        i ++;
+    if (buffer[i] == NULL)
+    {
+        free(buffer);
+        return (NULL);
+    }
+    line = ft_calloc_((ft_strlen_(buffer) - i + 1), sizeof(char));
+    i ++;
+    j = 0;
+    while (buffer[i])
+        line[j++] = buffer[i++];
+    free(buffer);
+    return (line);
+}
+
+static char *join_and_free(char *line_buf, char *chunk)
+{
+    char    *joined;
+
+    joined = ft_strjoin_(line_buf, chunk);
+    free(line_buf);
+    return (joined);
+}
+
+static char	*read_line(int fd, char *line_buf)
+{
+	char	*chunk;
+	int		octets_read;
+
+	if (line_buf == NULL)
+		line_buf = ft_calloc_(1, 1);
+	chunk = ft_calloc_(BUFFER_SIZE + 1, sizeof(char));
+	octets_read = 1;
+	while (octets_read > 0 )
+	{
+		octets_read = read(fd, chunk, BUFFER_SIZE);
+		if (octets_read == -1)
+		{
+			free(chunk);
+			return (NULL);
+		}
+		chunk[octets_read] = 0;
+		line_buf = join_and_free(line_buf, chunk);
+		if (strchr_(chunk, '\n'))
+			break;
+	}
+	free(chunk);
+	return (line_buf);
+}
+
+static char *extract_line(char *buffer)
+{
+	char	*line;
+	int		i;
+	
+	if (buffer == NULL)
+		return (NULL);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i ++;
+	line = ft_calloc_(i + 2, sizeof(char));
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i ++;
+	}
+	if (buffer[i] && buffer[i] == '\n')
+	{
+		i ++;
+		line[i] = '\n';
+	}
+	return (line);
+}
 
 char    *get_next_line(int fd)
 {
-    static size_t   read_octets = 0;
-    size_t          start;
-    size_t          octets_to_read;
-    char            *line;
+    static char *buffer;
+    char        *line;
 
-    octets_to_read = ;
-    start = read_octets;
-    read_octets += read(fd, line_buff, octets_to_read);
-    line = (char *)malloc((read_octets - start +1) * sizeof(char));
-    if (line == NULL)
+    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
         return (NULL);
-    line = line_cpy(line, src, start, read_octets);
+    buffer = read_line(fd, buffer);
+    if (buffer == NULL)
+        return (NULL);
+    line = extract_line(buffer);
+    buffer = update_buffer(buffer);
     return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,14 +1,57 @@
 #include "get_next_line.h"
 
-char	*line_copy(char *dest, char *src, size_t start, size_t end)
-{
-	int		i;
+//read(int fd, void *buf, size_t count)
 
-	i = start;
-	while (i < end)
+void	*ft_calloc_(size_t nelem, size_t elsize)
+{
+	int		*ptr;
+	size_t	i;
+
+	ptr = malloc(nelem * elsize);
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < nelem * elsize)
 	{
-		dest[i] = src[i];
+		ptr[i] = 0;
 		i ++;
 	}
-	return (dest);
+	return ((void *)ptr);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i ++;
+	return (i);
+}
+
+char	*ft_strjoin_(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	size_t	size;
+	char	*result;
+
+	size = ft_strlen(s1) + ft_strlen(s2);
+	result = (char *)malloc((size + 1) * sizeof(char));
+	if (result == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		result[i] = s1[i];
+		i ++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		result[i + j] = s2[j];
+		j ++;
+	}
+	result[i + j] = '\0';
+	return (result);
 }
